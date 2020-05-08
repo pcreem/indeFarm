7e1520acf6c718e29c9e8ff6805e99683f8f0e2c
pcreem <creempepper@gmail.com>
2020-05-08 14:58:01 +0800

    dashboard authenticated setup

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
-
 const db = require('./models')
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
+
 const app = express()
 const port = 3000
 
@@ -14,22 +14,25 @@ app.engine('.hbs', handlebars({
   extname: '.hbs',
   defaultLayout: 'main'
 }));
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }))
-
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
+
 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = req.user
   next()
 })
-app.use(passport.initialize())
-app.use(passport.session())
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+  db.sequelize.sync()
+  console.log(`Example app listening on port ${port}`)
 })
 
 require('./routes')(app, passport)
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,26 @@ const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
 
 module.exports = (app, passport) => {
+
+  const authenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/signin')
+  }
+  const authenticatedAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if (req.user.role) { return next() }
+      return res.redirect('/')
+    }
+    res.redirect('/signin')
+  }
+
   app.get('/', (req, res) => res.redirect('/index'))
   app.get('/index', frontController.getIndex)
 
-  app.get('/admin', (req, res) => res.redirect('/admin/dashboard'))
-  app.get('/admin/dashboard', adminController.getDashboard)
+  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/dashboard'))
+  app.get('/admin/dashboard', authenticatedAdmin, adminController.getDashboard)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
