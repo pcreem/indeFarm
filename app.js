5512782fe8426ffd3ecefae91127365ad87db4a8
pcreem <creempepper@gmail.com>
2020-05-08 13:20:21 +0800

    signin function

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const db = require('./models')
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const app = express()
 const port = 3000
 
@@ -24,9 +25,11 @@ app.use((req, res, next) => {
   res.locals.error_messages = req.flash('error_messages')
   next()
 })
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,16 @@ const frontController = require('../controllers/frontController.js')
 const adminController = require('../controllers/adminController.js')
 const userController = require('../controllers/userController.js')
 
-module.exports = app => {
+module.exports = (app, passport) => {
   app.get('/', (req, res) => res.redirect('/index'))
   app.get('/index', frontController.getIndex)
 
-  app.get('/signup', userController.signUpPage)
-  app.post('/signup', userController.signUp)
-
   app.get('/admin', (req, res) => res.redirect('/admin/dashboard'))
   app.get('/admin/dashboard', adminController.getDashboard)
+
+  app.get('/signup', userController.signUpPage)
+  app.post('/signup', userController.signUp)
+  app.get('/signin', userController.signInPage)
+  app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+  app.get('/logout', userController.logout)
 }
