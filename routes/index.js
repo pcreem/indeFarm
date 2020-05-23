94b21d6d164f0eafec138268b113a820385c8554
pcreem <creempepper@gmail.com>
2020-05-23 18:35:21 +0800

    organize route arrangement, add logout button

--- routes/index.js
+++ routes/index.js
@@ -30,15 +30,17 @@ module.exports = (app, passport) => {
   app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
   app.get('/logout', userController.logout)
 
-  app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/dashboard'))
-  app.get('/admin/dashboard', authenticatedAdmin, adminController.getDashboard)
-  app.get('/admin/agrifoods/create', authenticatedAdmin, adminController.getAgrifoodCEpage)
-  app.post('/admin/agrifoods', authenticatedAdmin, upload.single('image'), adminController.postAgrifood)
-  app.get('/admin/agrifoods/:id', authenticatedAdmin, adminController.getAgrifood)
-  app.get('/admin/agrifoods/:id/edit', authenticatedAdmin, adminController.editAgrifood)
-  app.put('/admin/agrifoods/:id', authenticatedAdmin, upload.single('image'), adminController.putAgrifood)
-  app.delete('/admin/agrifoods/:id', authenticatedAdmin, adminController.deleteAgrifood)
+  //both admin and user
+  app.get('/admin', authenticated, (req, res) => res.redirect('/admin/dashboard'))
+  app.get('/admin/dashboard', authenticated, adminController.getDashboard)
+  app.get('/admin/agrifoods/create', authenticated, adminController.getAgrifoodCEpage)
+  app.post('/admin/agrifoods', authenticated, upload.single('image'), adminController.postAgrifood)
+  app.get('/admin/agrifoods/:id', authenticated, adminController.getAgrifood)
+  app.get('/admin/agrifoods/:id/edit', authenticated, adminController.editAgrifood)
+  app.put('/admin/agrifoods/:id', authenticated, upload.single('image'), adminController.putAgrifood)
+  app.delete('/admin/agrifoods/:id', authenticated, adminController.deleteAgrifood)
 
+  //admin only
   app.get('/admin/members', authenticatedAdmin, adminController.getMembers)
   app.put('/admin/members/:id', authenticatedAdmin, adminController.putApproved)
 
