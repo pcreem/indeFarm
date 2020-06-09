46284da4ab30ff2baa1c0053e4b77612f6bb24bf
pcreem <creempepper@gmail.com>
2020-06-09 22:57:31 +0800

    sequlize index modify for heroku

--- models/index.js
+++ models/index.js
@@ -9,7 +9,10 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.url) {
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config)
+}
+else if (config.url) {
   sequelize = new Sequelize(config.url, config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
